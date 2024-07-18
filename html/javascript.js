3d756b32fa9c070eb14a1a9593f2957ee29f1ca8
wepdeveloper <mohammadedrissafawi66@gmail.com>
2024-07-18 14:57:47 +0300

    uppdate all file

--- html/javascript.js
+++ html/javascript.js
@@ -1,2 +1,9 @@
 
-console.log('hello')
+let  key = 'name';
+let obj = {
+    [key]: 'edris',
+};
+
+obj['name'] = 'ahmad'
+
+console.log(obj[key]);
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,46 @@ export default {
   ],
   theme: {
     extend: {
+      
 
       fontFamily: {
+
+        // refuler:['regular'],
 
         'Clash': ['Clash Display', 'sans-serif'],
         'Bony': ['Bonny', 'serif'],
         'Panchang': ['Panchang', 'sans-serif'],
         'Britney': ['Britney', 'cursive'],
         'Melodrama': ['Melodrama', 'serif'],
-        'Boxing': ['Boxing', 'sans-serif'],
-        'Switzer': ['Switzer', 'sans-serif'],
-        'Plus': ['Zodiak', 'serif'],
-        'Technor': ['Supreme', 'sans-serif'],
         'Rowan': ['Quilon', 'sans-serif'],
+        'gamgote':['SupremelyLuxurious'],
 
+        //custom font
+        'regular':['Regular'],
+        'fogtwo':['FogtwoNo5'],
+        'adoria':['adoria'],
+        'brolink':['brolink'],
+        'dopeDisplay':['dopeDisplay'],
+        'bestogram': ['bestogram'],
+        'enigma': ['enigma'],
+        'boruna': ['boruna'],
+        'maginors': ['maginors'],
+        'adora': ['adoria'],
+        'dingers': ['dingers'],
+        'major': ['major'],
+        'bodware': ['bodware'],
+        'saudager': ['saudager'],
+        'fungka': ['fungka'],
+        'falling': ['falling'],
+        'alt': ['alt'],
+        'jasmine': ['jasmine'],
+        'greyet': ['greyet'],
+        'raisa': ['raisa'],
+        'vako': ['vako'],
+        'Blue': ['Blue'],
+        'blushing': ['blushing'],
+
+        
       },
 
 
@@ -27,6 +53,11 @@ export default {
 
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar')({ nocompatible: true }),
+    // require('tailwind-scrollbar')({ preferredStrategy: 'pseudoelements' }),  // default: 'standard'
+    // require('tailwind-scrollbar'),
+
+  ],
 }
 
